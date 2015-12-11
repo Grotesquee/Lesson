a6e4a86eb3bd2085237ae1738f5f68a659619e0f
Grotesquee <grotesquepwnz@gmail.com>
2015-12-11 20:24:22 +0200

    ООП Имплементация интерфейсов. неймспейсы + homework

--- index2.php
+++ index2.php
@@ -2,20 +2,39 @@
 require_once 'vendor/autoload.php';
 
 $obj = new Lesson\Car\CarAudiClass();
-var_dump($obj->model(), $obj->speed());
-
-$obj3 = new Lesson\Car\CarVolvoClass();
-var_dump($obj3->model(), $obj3->speed());
+if ($obj instanceof \Lesson\Car\CarInterface) {
+    var_dump($obj->model(), $obj->speed());
+}
 
 $obj2 = new \Lesson\Boat\BoatClass();
 var_dump($obj2->model());
 
+$obj3 = new Lesson\Car\CarVolvoClass();
+if ($obj3 instanceof \Lesson\Car\CarInterface) {
+    var_dump($obj3->model(), $obj3->speed());
+}
 
 
 
 
+/*в папке \src создать новый namespace, подключить его в composer.json, назвать его Validator.
+создать ValidateIterface, в нем создать публичный метод isValid($value) (после проверки метод должен вернуть true or false )
+ и создать 2 класса, имплементирующие интерфейс ValidatorIntClass, ValidatorStringClass
 
 
+ValidatorIntClass должен проверять, что $value является числом
+ValidatorStringClass должен проверять, что $value является строкой
+
+index.
+$validator = new ValidateStringClass();
+$result = $validator->isValid('string');
+var_dump($result); //true
+$result = $validator->isValid(2);
+var_dump($result); //false
+
+
+
+*/
 
 
 
